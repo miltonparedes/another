ec6f9e98ccf3a3fd58d99832ee609da362b415e5
Milton Olaf <miltonolaf@outlook.com>
2019-08-15 12:49:19 -0500

    Impv. mobile navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,28 +12,31 @@ setTheme(theme);
 initTheme("light", "dark");
 initTheme("dark", "light");
 
-//Exelent little functions to use any time when class modification is needed
+// Exelent little functions to use any time when class modification is needed
 function hasClass(ele, cls) {
   return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
 }
 
 function addClass(ele, cls) {
-  if (!hasClass(ele, cls)) ele.className += " " + cls;
+  if (!hasClass(ele, cls)) ele.className += "" + cls;
 }
 
 function removeClass(ele, cls) {
   if (hasClass(ele, cls)) {
       var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
-      ele.className = ele.className.replace(reg, ' ');
+      ele.className = ele.className.replace(reg, '');
   }
 }
 
-//Add event from js the keep the marup clean
+// Add event from js the keep the markup clean
 function init() {
-  document.getElementById("menu-toggle").addEventListener("click", toggleMenu);
+  let elementsArray = document.querySelectorAll(".menu-toggle");
+  elementsArray.forEach(function(element) {
+    element.addEventListener("click", toggleMenu);
+  });
 }
 
-//The actual fuction
+// The actual function
 function toggleMenu() {
   var ele = document.getElementsByTagName('body')[0];
   if (!hasClass(ele, "open")) {
@@ -43,7 +46,7 @@ function toggleMenu() {
   }
 }
 
-//Prevent the function to run before the document is loaded
+// Prevent the function to run before the document is loaded
 document.addEventListener('readystatechange', function() {
   if (document.readyState === "complete") {
       init();
